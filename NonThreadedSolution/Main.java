b4330527b3c4dca67353b71e26dd725e59672d1b
urja jindal <44872768+urjajindal18@users.noreply.github.com>
2022-11-25 22:29:40 -0500

    Create Main.java

---
+++ NonThreadedSolution/Main.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class AssignmentOne {
+	/**
+	 * We are just calling the first menu here.
+	 */
+  
+	public static void main(String[] args) {
+		
+		MenuOne.menuOne();
+	}
+	
+}
