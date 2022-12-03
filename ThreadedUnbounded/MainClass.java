369b2bbaba5ba2e1b1aff57158d66ec40c1b2dbf
urja jindal <44872768+urjajindal18@users.noreply.github.com>
2022-12-02 21:36:06 -0500

    Create MainClass.java

---
+++ ThreadedUnbounded/MainClass.java
@@ -0,0 +1,18 @@
+package concAssignment;
+
+
+
+public class MainClass {
+	/**
+	 * Here we call the menu one which prints the options for factorization
+	 * @param args
+	 * @throws InterruptedException
+	 */
+
+public static void main(String[] args) throws InterruptedException {
+		
+	
+		MenuOne.menuOne();
+	}
+
+}
